ef8ff059efb139a564db2490212ac101e7562752
RajiAujla <Rajwinderaujla061@gmail.com>
2021-11-27 23:30:51 -0500

    working the api test in swagger

--- restapi/src/main/java/com/example/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/example/restapi/RestapiApplication.java
@@ -2,12 +2,20 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/youtube-services");
 		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RestapiApplication.class);
 	}
 
 }
--- restapi/src/main/java/com/example/restapi/controller/TestingController.java
+++ restapi/src/main/java/com/example/restapi/controller/TestingController.java
@@ -1,5 +1,19 @@
 package com.example.restapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
 public class TestingController {
-    
+
+    @ApiOperation(value = "Testing")
+    @RequestMapping(value = "/testing", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Boolean testing() {
+        return true;
+    }
+
 }
